cd757ca8a3c6e575d96b948fe0bea5723de389cc
NA anna <laanna1001@gmail.com>
2023-03-28 14:58:37 +0900

    카카오 맵 API 설정

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -6,7 +6,49 @@
 
 <script>
 export default {
+    data () {
+        return
+    },
+    mounted() {
+        const API_KEY = 'f743b18ec927badfec1f4a446e0f3155';
 
+        if (window.kakao && window.kakao.maps) {
+            this.initMap();
+        } else {
+            const script = document.createElement('script');
+            /* global kakao */
+            script.onload = () => kakao.maps.load(this.initMap);
+            script.src =
+                `http://dapi.kakao.com/v2/maps/sdk.js?autoload=false&appkey=${API_KEY}`;
+            document.head.appendChild(script);
+        }
+    },
+    methods: {
+
+        initMap() {
+            var mapContainer = document.getElementById('map'), // 지도를 표시할 div
+                mapOption = {
+                    center: new kakao.maps.LatLng(37.564343, 126.947613), // 지도의 중심좌표
+                    level: 13, // 지도의 확대 레벨
+                };
+            var map = new kakao.maps.Map(mapContainer, mapOption);
+            var positions = [
+                {
+                    latlng: new kakao.maps.LatLng(37.562632898194835, 126.9454282268269),
+                },
+            ]
+
+            // 마커를 생성합니다
+            positions.forEach(function(pos) {
+                var marker = new kakao.maps.Marker({
+                    position: pos.latlng, // 마커의 위치
+                })
+
+                //마커가 지도 위에 표시되도록 설정합니다.
+                marker.setMap(map)
+            })
+        },
+    },
 }
 </script>
 
